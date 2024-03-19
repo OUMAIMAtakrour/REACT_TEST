20a69ba2b9614583fff52508bec87c327008d26b
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-03-19 18:48:01 +0000

    external internal and inline styling

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./card.jsx";
 import Button from "./Button/button.jsx";
+import Button2 from "./Button2.jsx";
 function App() {
 return(
   // <> 
@@ -14,7 +15,8 @@ return(
   // <Card></Card>
   // <Card></Card>
   // </>
-<Button/>
+  
+<Button2/>
 
 );
 }
---
+++ my-react-app/src/Button2.jsx
@@ -0,0 +1,22 @@
+
+function Button2(){
+
+    const styles={
+        
+            backgroundColor:' hsl(155, 100%, 50%)',
+            color: 'white',
+            padding:' 20px 30px',
+            borderRadius: '50px',
+            border:' none',
+            cursor: 'help',
+        
+    }
+    return(
+
+    <button style={styles}>Click Me</button>
+    );
+    
+    
+    }
+    
+    export default Button2
